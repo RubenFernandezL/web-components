038be02ffcae9d59e5ef55ba874bdb7be183423c
Rubén Fernández Lopesino <ruben280994@gmail.com>
2023-03-03 09:04:34 +0100

    Extending specific elements

---
+++ confirm-link.js
@@ -0,0 +1,9 @@
+class ConfirmLink extends HTMLAnchorElement {
+  connectedCallback() {
+    this.addEventListener("click", (event) => {
+      if (!confirm("Do you really want to leave?")) event.preventDefault();
+    });
+  }
+}
+
+customElements.define("rfl-confirm-link", ConfirmLink, { extends: "a" });
