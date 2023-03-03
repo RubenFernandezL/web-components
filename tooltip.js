692fd40f9c74c9b16361d3eea81167fd87790c91
Rubén Fernández Lopesino <ruben280994@gmail.com>
2023-03-03 08:56:45 +0100

    Tooltip with shadow dom

--- tooltip.js
+++ tooltip.js
@@ -1,30 +1,53 @@
 class Tooltip extends HTMLElement {
+  _tooltipContainer;
+  _tooltipText;
+  _tooltipIcon;
+
   //Basic initializations
   constructor() {
     super();
-    this.tooltipContainer = document.createElement("div");
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.innerHTML = `<slot>Some default</slot><span>(?)</span>`;
+    this._tooltipContainer = document.createElement("div");
+    this._tooltipText = "";
   }
+
   //Here the component is loaded into de DOM and it's possible to work with it
   connectedCallback() {
-    const icon = document.createElement("span");
-    icon.textContent = " (?)";
-    icon.addEventListener("mouseenter", this._showTooltip.bind(this));
-    icon.addEventListener("mouseleave", this._hideTooltip.bind(this));
-    this.appendChild(icon);
+    if (this.hasAttribute("text")) this._tooltipText = this._text;
+    this._addTooltipListeners();
   }
+
   //Element detached from DOM, cleanup work
   disconnectedCallback() {}
 
   //Observed attribute updated, update data + DOM
   attributeChangedCallback() {}
 
+  get _text() {
+    return this.getAttribute("text");
+  }
+
+  _addTooltipListeners() {
+    this._tooltipIcon = this.shadowRoot.querySelector("span");
+    this._tooltipIcon.addEventListener(
+      "mouseenter",
+      this._showTooltip.bind(this)
+    );
+    this._tooltipIcon.addEventListener(
+      "mouseleave",
+      this._hideTooltip.bind(this)
+    );
+    this.shadowRoot.appendChild(this._tooltipIcon);
+  }
+
   _showTooltip() {
-    this.tooltipContainer.textContent = "This is the tooltip text";
-    this.appendChild(this.tooltipContainer);
+    this._tooltipContainer.textContent = this._tooltipText;
+    this.shadowRoot.appendChild(this._tooltipContainer);
   }
 
   _hideTooltip() {
-    this.removeChild(this.tooltipContainer);
+    this.shadowRoot.removeChild(this._tooltipContainer);
   }
 }
 
